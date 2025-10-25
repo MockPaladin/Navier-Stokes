5856f8ad15b8fe8b854045305c54f1abf62519b3
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2025-10-24 20:33:07 -0500

    Made basic vector plot

--- Navier-Stokes_mConstant.py
+++ Navier-Stokes_mConstant.py
@@ -1,1 +1,20 @@
-print("AAAAAAAAAAAAAA")
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-np.pi, np.pi, 25)
+y = np.linspace(-np.pi, np.pi, 25)
+
+X, Y = np.meshgrid(x, y)
+
+U = np.sin(X)
+V = np.sin(Y)
+
+plt.quiver(X, Y, U, V, color='blue')
+
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('2D Vector Field Visualization')
+
+plt.axis('equal')
+
+plt.show()
