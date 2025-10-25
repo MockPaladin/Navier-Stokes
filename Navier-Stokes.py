cfca581f8a832ce1a09a10bf7e9667f3e204cbc5
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2025-10-25 12:41:54 -0500

    added gif and animation

--- Navier-Stokes.py
+++ Navier-Stokes.py
@@ -1,16 +1,20 @@
 from scipy.integrate import odeint # type: ignore
 import matplotlib.pyplot as plt # type: ignore
+from matplotlib.animation import FuncAnimation, PillowWriter # type: ignore
 import numpy as np # type: ignore
 import os
 
 x = np.linspace(-5, 5, 25)
 y = np.linspace(-5, 5, 25)
-t = np.linspace(0, 100, 10)
+t = np.linspace(0, 25, 20)
 
-X, Y, T = np.meshgrid(x, y, t)
+X, Y, T = np.meshgrid(x, y, t, indexing='ij')
 
 U = np.sin(Y) * T
 V = np.cos(X) * 2 * T
+
+U = np.where((U == 0) & (V == 0), 1e-12, U)
+V = np.where((U == 0) & (V == 0), 1e-12, V)
 
 dx = x[1] - x[0]
 dy = y[1] - y[0]
@@ -27,7 +31,7 @@ conv_v = U * dudx * dudy
 DuDt = dudt + conv_u
 DvDt = dvdt + conv_v
 
-for time in range(0, 10):
+"""for time in range(0, 10):
 
     plt.quiver(X[:, :, time], Y[:, :, time], DuDt[:, :, time], DvDt[:, :, time])
 
@@ -35,3 +39,19 @@ for time in range(0, 10):
     plt.ylabel('y')
     plt.title(f'Plot at t={time}')
     plt.savefig(os.path.join('images', f't{time}.png'))
+
+"""
+
+fig, ax = plt.subplots()
+quiv = ax.quiver(X[:, :, 0], Y[:, :, 0], DuDt[:, :, 0], DvDt[:, :, 0], angles='xy', scale_units='xy', scale=1e2)
+
+def update(frame):
+
+    quiv.set_UVC(U[:, :, frame], V[:, :, frame])
+    ax.set_title(f"t={t[frame]:.2f}")
+    return quiv,
+
+animation = FuncAnimation(fig, update, frames=len(t), interval=300, blit=True)
+plt.show()
+
+animation.save("sim.gif", writer=PillowWriter(fps=10), dpi=150)
