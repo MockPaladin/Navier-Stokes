a3dfcc2de0ea02ddfa23bdd24ffd0075d8e0c6c9
Alex Reutiman <155388978+MockPaladin@users.noreply.github.com>
2025-10-24 20:56:11 -0500

    Starting vector field
    Added source code for the starting vector field, and the image generated by it.

---
+++ Starting-Vector-Field.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(-5, 5, 25)
+y = np.linspace(-5, 5, 25)
+
+X, Y = np.meshgrid(x, y)
+
+U = np.sin(Y)
+V = np.cos(X)
+
+plt.quiver(X, Y, U, V, color='black')
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Starting vector field')
+
+plt.axis('equal')
+
+plt.show()
